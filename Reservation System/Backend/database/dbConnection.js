68cb7938bd34d20d1e6d11824dbea60cf082e6b9
SarthakDhiman446 <sarthakdhiman904@gmail.com>
2026-04-19 20:57:57 +0530

    Create dbConnection.js

---
+++ Reservation System/Backend/database/dbConnection.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const dbConnection = () => {
+    mongoose.connect(process.env.MONGO_URI, {
+        dbName: "RESERVATIONS",
+    })
+    .then(() => console.log("Connected to database!"))
+    .catch((err) => {
+        console.log(`Some error occured while connecting to database: ${err}`);
+    });
+};
+
+export default dbConnection;
